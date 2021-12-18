f72977715ceaa8c493b431fe61345ea78af0c15b
ainnodb <motingxia@163.com>
2021-12-18 20:16:46 +0800

    remvoe db_config_db.py

--- db_config_dba.py
+++
@@ -1,9 +0,0 @@
-import cx_Oracle
-username=""
-password=""
-host=""
-port=1521
-instance=""
-tns = cx_Oracle.makedsn(host,port,instance)
-con=cx_Oracle.connect(username,password,tns, mode=cx_Oracle.SYSDBA, encoding="UTF-8" ,nencoding="UTF-8")
-
